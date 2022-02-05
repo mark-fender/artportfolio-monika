f613c0fe3e52e959b411b48ccdb1b54e80f0fa16
Marek Lipčák <marek.lipcak@workflows.sk>
2022-02-05 20:21:26 +0100

    fetching data from firebase

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,27 @@
+import { useEffect, useState } from "react";
+import { db } from "./../firebase-config";
+import { collection, getDocs } from "firebase/firestore";
+import "./App.css";
+
 function App() {
+  const [series, setSeries] = useState([]);
+  const seriesCollectionRef = collection(db, "series");
+  useEffect(() => {
+    const getSeries = async () => {
+      const series = await getDocs(seriesCollectionRef);
+      setSeries(series.docs.map((doc) => ({
+        ...doc.data(), id: doc.id
+      })));
+    }
+    getSeries();
+  }, []);
   return (
     <div className="App">
-      HELLO WORLD!
+      {series.map((serie) => { 
+        return <div>
+          <span>Name: {serie.name}</span>
+        </div>
+      })}
     </div>
   );
 }
