aee2a9be63b8f26e7b2dabbd66089781bb1284ec
Marek Lipčák <marek.lipcak@workflows.sk>
2022-05-01 15:06:15 +0200

    test

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -4,12 +4,12 @@ import "firebase/compat/firestore";
 import "firebase/compat/storage";
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  apiKey: "AIzaSyCb9xQKKx-_dkRG70rVAXnzk0iCuNkWloU",
+  authDomain: "artportfolio-monika-prod.firebaseapp.com",
+  projectId: "artportfolio-monika-prod",
+  storageBucket: "artportfolio-monika-prod.appspot.com",
+  messagingSenderId: "475563240044",
+  appId: "1:475563240044:web:12cb7f29af21d15aa14649",
 };
 
 // Initialize Firebase
