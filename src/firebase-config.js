34c59f90ec4c5d97727ecd205453351ef462573b
Marek Lipčák <marek.lipcak@workflows.sk>
2022-05-01 12:18:57 +0200

    config

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -2,14 +2,15 @@ import firebase from "firebase/compat/app";
 import "firebase/compat/auth";
 import "firebase/compat/firestore";
 import "firebase/compat/storage";
+import * as functions from "firebase/functions";
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  apiKey: functions.artportfolio_monika.apiKey,
+  authDomain: process.env.artportfolio_monika.auth_domain,
+  projectId: process.env.artportfolio_monika.project_id,
+  storageBucket: process.env.artportfolio_monika.storage_bucket,
+  messagingSenderId: process.env.artportfolio_monika.messaging_sender_id,
+  appId: process.env.artportfolio_monika.app_id,
 };
 
 // Initialize Firebase
