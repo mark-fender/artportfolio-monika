436ee3b01da865fa881b7efad4fe62ca39e7bb59
Marek Lipčák <marek.lipcak@workflows.sk>
2022-06-21 17:35:32 +0200

    removed anon auth

--- src/components/admin/AdminPage.js
+++ src/components/admin/AdminPage.js
@@ -55,7 +55,6 @@ export default function AdminPage() {
 
   async function logout() {
     await auth.signOut();
-    await auth.signInAnonymously();
   }
 
   function setSelectedPaintingImage(e) {
